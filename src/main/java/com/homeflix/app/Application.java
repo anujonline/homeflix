f568b9b6de769880cf45f0312234254c5c857e43
anuj <anujbookman@gmail.com>
2023-08-10 06:48:32 +0200

    exception logging

--- src/main/java/com/homeflix/app/Application.java
+++ src/main/java/com/homeflix/app/Application.java
@@ -8,18 +8,17 @@ import com.vaadin.flow.component.page.Meta;
 import com.vaadin.flow.component.page.Viewport;
 import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
-import com.vaadin.flow.theme.lumo.Lumo;
 import com.vaadin.flow.theme.material.Material;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
+import java.util.Arrays;
 
 /**
  * The entry point of the Spring Boot application.
@@ -39,23 +38,32 @@ import java.net.URI;
 @EnableScheduling
 public class Application implements AppShellConfigurator {
 
+    private static final RestTemplate REST_TEMPLATE = new RestTemplate();
+    @Autowired
+    private MovieRepository movieRepository;
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-private static final RestTemplate REST_TEMPLATE=new RestTemplate();
+
+    private static String createIdentifier(String url) {
+        var uri = URI.create(url);
+        var split = uri.getPath().split("/");
+        return split[2];
+    }
+
     @Scheduled(fixedDelay = 10000L)
-    void call(){
+    void call() {
         var response = REST_TEMPLATE.getForEntity("https://homeflix.onrender.com", String.class);
         System.out.println(response.getStatusCode());
     }
-    @Autowired private MovieRepository movieRepository;
+
     @PostConstruct
-    void addMovies(){
-//        saveMovie("Bhabhi ji ghar pe hai", "https://s2.videoapne.co/hls/bdohxxyi7bboxuzvtarp4hqc2yjwuksztev7hyv4zez6hdkvrdvktsup5ioa/index-v1-a1.m3u8");
-        saveMovie("Hunt of Veerappan epidode 1", "https://s2.videoapne.co/hls/bdohwiai7bboxuzvtarp4cacqkaou3rxfafwhmvaxvqb4nvp5qkr42gv3ixq/index-v1-a1.m3u8","https://im.indiatimes.in/content/itimes/photo/2016/Oct/18/1476781289-most-unseen-photos-of-veerappan_card.jpg?w=502&h=334&cc=1");
-        saveMovie("Little Mermaid", "https://eerht.artdesigncdn.net/_v10/165ffb574f2d1baf64d5e0a7fabb9961f40581c41058d15d4d9a07b766853eef6560ec5d1b88da900ca632875404ea0413ed9f72a6ec06ce086348ebe03f97fd4b276180a6099d4bf8a0eadef73146e150534fcbe86f0419b09feb05136aad24ea31883549e52eb3abfb86c1dea4b106867b165a20f55ead06722cd2dcaf5e551f68816194d5c8a1661a455c923b033d/720/index.m3u8","https://media-cache.cinematerial.com/p/500x/u0a2cwcc/little-mermaid-movie-poster.jpg?v=1479369946");
-        saveMovie("Oppenheimer","https://xis.fifteennet.com/_v10/0db3086e7a22f9ed3b28cde10c2ebd49454fbf30f0a3acca80dc4a0e67d9493b7522ca49e837e91d14929942b074b49fc751e8f52948d9619cdceff79161cdd57ef9535b4b00468f85188e51527b849a0dbefc3956afe559a8adaf6fc8589fb62d87bcd5db5903dd0273d3138b2649d990881c4b2c04910ae242d2e8a927cc4999bf543291abdd0062c8d28608331831/720/index.m3u8","https://media-cache.cinematerial.com/p/500x/2afqhdxx/oppenheimer-movie-poster.jpg?v=1683305737");
-        saveMovie("Before Sunrise","https://neves.jeffycontent.com/_v10/d7b50b552eb03ef2ed80bc8610b845c7ed6fbdf7d4ae29e4076216cc826f3e16c7804aecbf1fe0298cd15b34a8ce013d24d78d9d2bfa09ed317fc6f9a8ab79472b23002298aba8a380806a756a1ed541959a524647fac1af738b8c834d263fc90bd7676a700f10065154bbe27566b28658a918b15bbe9c8f02ff3a6d3fa5264dab206a5b29e8926b0cf2188219a36c2d/playlist.m3u8","https://media-cache.cinematerial.com/p/500x/at0bkxjz/before-sunrise-movie-poster.jpg?v=1456716574");
+    void addMovies() {
+        saveMovie("Little Mermaid", "https://eerht.artdesigncdn.net/_v10/165ffb574f2d1baf64d5e0a7fabb9961f40581c41058d15d4d9a07b766853eef6560ec5d1b88da900ca632875404ea0413ed9f72a6ec06ce086348ebe03f97fd4b276180a6099d4bf8a0eadef73146e150534fcbe86f0419b09feb05136aad24ea31883549e52eb3abfb86c1dea4b106867b165a20f55ead06722cd2dcaf5e551f68816194d5c8a1661a455c923b033d/720/index.m3u8", "https://media-cache.cinematerial.com/p/500x/u0a2cwcc/little-mermaid-movie-poster.jpg?v=1479369946");
+        saveMovie("Man from UNCLE", "https://eerht.artdesigncdn.net/_v10/c21283078281e4cbd1bfa4a356d7bf29a50a6dc6880b1cab2bbf7a60f8758afa831765638d5abbbd79b086db4a8fa7c99bffc882f67a6360002571a9bbb05423138d3cbedddf78fb743fa71e3f782d23f7b202f71f7d51b388d153fdc225367b860f452d333419316ac27b6ecafd7da3473ae970618eb4a1505b43369c0aaee1a6499a64a2b8ab40f402b34fcfa0262f/playlist.m3u8", "https://www.moviemeter.nl/images/cover/100000/100866.jpg?cb=1460039091");
+        saveMovie("Oppenheimer", "https://xis.fifteennet.com/_v10/0db3086e7a22f9ed3b28cde10c2ebd49454fbf30f0a3acca80dc4a0e67d9493b7522ca49e837e91d14929942b074b49fc751e8f52948d9619cdceff79161cdd57ef9535b4b00468f85188e51527b849a0dbefc3956afe559a8adaf6fc8589fb62d87bcd5db5903dd0273d3138b2649d990881c4b2c04910ae242d2e8a927cc4999bf543291abdd0062c8d28608331831/720/index.m3u8", "https://media-cache.cinematerial.com/p/500x/2afqhdxx/oppenheimer-movie-poster.jpg?v=1683305737");
+        saveMovie("Before Sunrise", "https://neves.jeffycontent.com/_v10/d7b50b552eb03ef2ed80bc8610b845c7ed6fbdf7d4ae29e4076216cc826f3e16c7804aecbf1fe0298cd15b34a8ce013d24d78d9d2bfa09ed317fc6f9a8ab79472b23002298aba8a380806a756a1ed541959a524647fac1af738b8c834d263fc90bd7676a700f10065154bbe27566b28658a918b15bbe9c8f02ff3a6d3fa5264dab206a5b29e8926b0cf2188219a36c2d/playlist.m3u8", "https://media-cache.cinematerial.com/p/500x/at0bkxjz/before-sunrise-movie-poster.jpg?v=1456716574");
     }
 
     private void saveMovie(String name, String url, String posterLink) {
@@ -64,13 +72,7 @@ private static final RestTemplate REST_TEMPLATE=new RestTemplate();
         movie.setIdentifier(createIdentifier(url));
         movie.setLink(URI.create(url));
         movie.setPosterLink(posterLink);
+        movie.setActive(true);
         movieRepository.save(movie);
-    }
-
-    private static String createIdentifier(String url){
-        var uri = URI.create(url);
-        var split = uri.getPath().split("/");
-        System.out.println(split);
-        return split[2];
     }
 }
--- src/main/java/com/homeflix/app/data/controllers/VideoController.java
+++ src/main/java/com/homeflix/app/data/controllers/VideoController.java
@@ -9,8 +9,7 @@ import java.net.URISyntaxException;
 import java.net.URLDecoder;
 import java.nio.charset.Charset;
 
-//https://s2.videoapne.co/hls/bdohxxyi7bboxuzvtarp4hqc2yjwuksztev7hyv4zez6hdkvrdvktsup5ioa/index-v1-a1.m3u8 --> metadata request
-//https://s2.videoapne.co/hls/bdohxxyi7bboxuzvtarp4hqc2yjwuksztev7hyv4zez6hdkvrdvktsup5ioa/seg-1-v1-a1.ts ----> video segment request
+
 @RestController
 @RequestMapping("/video")
 public class VideoController<T> {
@@ -20,18 +19,7 @@ public class VideoController<T> {
         return identifier.contains(".m3u8");
     }
 
-    @GetMapping("/identifier/{identifier}")
-    ResponseEntity<?> getsrc(@CookieValue("mainURL") String cookie, @PathVariable("identifier") String identifier) {
-        identifier = URLDecoder.decode(URLDecoder.decode(identifier, Charset.defaultCharset()), Charset.defaultCharset());
-        if (isMetaDataRequest(identifier)) {
-            return restTemplate.getForEntity(identifier, String.class);
-        } else {
-            var uri = generateURI(URI.create(cookie), identifier);
-            return restTemplate.getForEntity(uri, byte[].class);
-        }
-    }
-
-    private static URI generateURI(URI uri, String newFilename){
+    private static URI generateURI(URI uri, String newFilename) {
         try {
             String path = uri.getPath();
             int lastSlashIndex = path.lastIndexOf('/');
@@ -39,6 +27,22 @@ public class VideoController<T> {
             return new URI(uri.getScheme(), uri.getAuthority(), newPath, uri.getQuery(), uri.getFragment());
         } catch (URISyntaxException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    @GetMapping("/identifier/{identifier}")
+    ResponseEntity<?> getsrc(@CookieValue("mainURL") String cookie, @PathVariable("identifier") String identifier) {
+        try {
+            identifier = URLDecoder.decode(URLDecoder.decode(identifier, Charset.defaultCharset()), Charset.defaultCharset());
+            if (isMetaDataRequest(identifier)) {
+                return restTemplate.getForEntity(identifier, String.class);
+            } else {
+                var uri = generateURI(URI.create(cookie), identifier);
+                return restTemplate.getForEntity(uri, byte[].class);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(500).body(e.getMessage());
         }
     }
 }
