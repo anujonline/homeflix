6d56752cb4a53876df289131774704f5aa60ed41
anuj <anujbookman@gmail.com>
2023-11-09 23:19:45 +0100

    disable chrome

--- src/main/java/com/homeflix/app/views/common/MainLayout.java
+++ src/main/java/com/homeflix/app/views/common/MainLayout.java
@@ -1,50 +1,35 @@
 package com.homeflix.app.views.common;
 
+import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.contextmenu.ContextMenu;
-import com.vaadin.flow.component.icon.VaadinIcon;
-import com.vaadin.flow.dom.Element;
-import com.vaadin.flow.dom.Style;
+import com.vaadin.flow.component.dialog.Dialog;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 
 /**
  * The main view is a top-level placeholder for other views.
  */
 public class MainLayout extends AppLayout {
+    private static final String MESSAGE = "Chrome browser is not supported at this moment.";
     public MainLayout() {
         getStyle().set("border-radius", "25px");
         getStyle().set("overflow", "hidden");
-        var bellBtn = new MessagesButton();
-        bellBtn.setUnreadMessages(1);
-
-        var menu = new ContextMenu();
-        menu.setOpenOnClick(true);
-        menu.setTarget(bellBtn);
-        menu.addItem("Chrome issue is now resolved, you can continue using your favourite browser");
-
-        addToNavbar(bellBtn);
-    }
-
-    private static class MessagesButton extends Button {
-
-        private final Element numberOfNotifications;
-
-        private MessagesButton() {
-            super(VaadinIcon.BELL_O.create());
-            numberOfNotifications = new Element("span");
-            numberOfNotifications.getStyle().setPosition(Style.Position.ABSOLUTE).setTransform("translate(-40%, -85%)");
-
-        }
-
-        void setUnreadMessages(int unread) {
-            numberOfNotifications.setText(unread + "");
-            if (unread > 0 && numberOfNotifications.getParent() == null) {
-                getElement().appendChild(numberOfNotifications);
-            } else if (numberOfNotifications.getNode().isAttached()) {
-                numberOfNotifications.removeFromParent();
+        var ui = UI.getCurrent();
+        var current = ui.getSession();
+        ui.getPage().retrieveExtendedClientDetails(extendedClientDetails -> {
+            if (current.getBrowser().isChrome() && !extendedClientDetails.isTouchDevice()) {
+                addToNavbar(new Marquee(MESSAGE));
+                if (current.getAttribute("message-read") == null) {
+                    var show = new Dialog();
+                    show.add(new VerticalLayout(new Text(MESSAGE), new Button("Ok", event -> show.close())));
+                    show.open();
+                    current.setAttribute("message-read", true);
+                }
             }
-        }
+        });
 
     }
 }
+
 
