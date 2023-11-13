5fcb0d6d9b6f7539cc2ece177ab09ca2ee8c6dc5
anuj <anujbookman@gmail.com>
2023-11-13 18:30:38 +0100

    homeflix-1 change streamer

--- src/main/java/com/homeflix/app/views/Embed.java
+++ src/main/java/com/homeflix/app/views/Embed.java
@@ -9,15 +9,21 @@ public class Embed extends HtmlContainer {
     private final PropertyDescriptor<String, String> srcDescriptor = PropertyDescriptors.attributeWithDefault("src", "");
 
     public Embed() {
+        setId("hframe");
         setWidthFull();
         setHeight("70%");
         if (!UI.getCurrent().getSession().getBrowser().isChrome()) {
-            getElement().setProperty("sandbox", "allow-forms allow-pointer-lock allow-same-origin allow-scripts");
+            getElement().setProperty("sandbox", "");
         }
+        getElement().setProperty("referrerpolicy","same-origin");
+        getElement().setAttribute("referrerpolicy","same-origin");
         getElement().setProperty("allow", "autoplay");
         getElement().setProperty("frameborder", "0");
         getElement().setProperty("allowfullscreen", "true");
         getElement().setAttribute("allowfullscreen", "true");
+        getElement().addEventListener("click", domEvent -> {
+            domEvent.getEventTarget().ifPresent(System.out::println);
+        });
     }
 
     public String getSrc() {
