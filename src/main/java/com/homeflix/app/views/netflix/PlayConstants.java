63482678c034e98877aea6716fbcbb55c88eee7e
anuj <anujbookman@gmail.com>
2024-02-18 18:57:46 +0100

    homeflix-9 enable metrics

--- src/main/java/com/homeflix/app/views/netflix/PlayConstants.java
+++ src/main/java/com/homeflix/app/views/netflix/PlayConstants.java
@@ -16,7 +16,7 @@ import java.util.function.Function;
 import static com.vaadin.flow.server.VaadinSession.getCurrent;
 
 public final class PlayConstants {
-    public static final String PLAY_URL = "https://vidsrc.to/embed/%s/%s";
+    public static final String PLAY_URL = "https://vidsrc.in/embed/%s/%s";
     public static final String POSTER_URL = "https://image.tmdb.org/t/p/w200/%s";
     public static final String REMOTE_VIEWING_IS_STILL_IN_BETA = "Remote Viewing is still in Beta";
 
