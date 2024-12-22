9336b91a03ec109a1aa623b5b55151f796a32ed2
anuj <anujbookman@gmail.com>
2024-12-22 17:47:44 +0100

    homeflix-16 remove history

--- src/main/java/com/homeflix/app/views/netflix/PlayUI.java
+++ src/main/java/com/homeflix/app/views/netflix/PlayUI.java
@@ -6,6 +6,7 @@ import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.Anchor;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
@@ -21,21 +22,26 @@ public class PlayUI extends VerticalLayout {
     private final Div div = new Div();
 
     public PlayUI() {
-        setSizeFull();
-        var anchor = new Anchor("https://brave.com/download/");
-        anchor.setText("For ad free experience we recommend using Brave Browser");
-        anchor.setClassName("Button");
-        anchor.setTarget(BLANK);
-        add(anchor);
-        embed.setSrc((String) VaadinSession.getCurrent().getAttribute("url"));
-        setAlignItems(FlexComponent.Alignment.CENTER);
-        var closeDialog = new Button("Back", VaadinIcon.ARROW_LEFT.create(), e -> {
-            UI.getCurrent().getPage().getHistory().back();
-        });
-        closeDialog.setWidthFull();
-        embed.setSizeFull();
-        div.add(closeDialog, embed);
-        div.setWidthFull();
-        add(div);
+        try {
+            setSizeFull();
+            var anchor = new Anchor("https://brave.com/download/");
+            anchor.setText("For ad free experience we recommend using Brave Browser");
+            anchor.setClassName("Button");
+            anchor.setTarget(BLANK);
+            add(anchor);
+            embed.setSrc((String) VaadinSession.getCurrent().getAttribute("url"));
+            setAlignItems(FlexComponent.Alignment.CENTER);
+            var closeDialog = new Button("Back", VaadinIcon.ARROW_LEFT.create(), e -> {
+                UI.getCurrent().getPage().getHistory().back();
+            });
+            closeDialog.setWidthFull();
+            embed.setSizeFull();
+            div.add(closeDialog, embed);
+            div.setWidthFull();
+            add(div);
+        } catch (Exception e) {
+            UI.getCurrent().navigate("");
+            Notification.show("There seems to be an error in the app, please report this via instagram on homepage");
+        }
     }
 }
